09fb17fcf4d0f1d802c87da710cffb726d744e6c
Airtune <airtune@protonmail.com>
2021-10-26 22:52:27 +0200

    Add MutexManager

---
+++ src/mutex-manager.ts
@@ -0,0 +1,29 @@
+import { Mutex } from 'async-mutex';
+
+export class MutexManager {
+  private mainMutex: Mutex;
+  private mutexByID: { string: Mutex }; 
+
+  constructor() {
+    this.mainMutex = new Mutex();
+    this.mutexByID = {} as { string: Mutex };
+  }
+
+  async runExclusive(id, func) {
+    // create mutex
+    await this.mainMutex.runExclusive(() => {
+      if (!(this.mutexByID[id] instanceof Mutex)) {
+        this.mutexByID[id] = new Mutex();
+      }
+    });
+
+    await this.mutexByID[id].runExclusive(func);
+
+    // cleanup mutex
+    await this.mainMutex.runExclusive(() => {
+      if (!this.mutexByID[id].isLocked()) {
+        delete this.mutexByID[id];
+      }
+    })
+  }
+}
