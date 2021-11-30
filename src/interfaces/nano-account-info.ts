3a7f17e94948e24a26c7f89b5cd1432500592ea4
Airtune <airtune@protonmail.com>
2021-11-30 20:41:04 +0100

    Create nano-account-info.ts

---
+++ src/interfaces/nano-account-info.ts
@@ -0,0 +1,11 @@
+export interface INanoAccountInfo {
+  frontier: string,
+  open_block: string,
+  representative_block: string,
+  balance: string,
+  modified_timestamp: string,
+  block_count: string,
+  account_version: string,
+  confirmation_height: string, 
+  confirmation_height_frontier: string
+}
