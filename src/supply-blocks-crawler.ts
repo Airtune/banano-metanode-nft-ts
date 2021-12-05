0ddeefe75fbdced3ab56aa624d22c5088763b541
Airtune <airtune@protonmail.com>
2021-12-05 04:11:59 +0100

    Update supply-blocks-crawler.ts

--- src/supply-blocks-crawler.ts
+++ src/supply-blocks-crawler.ts
@@ -5,7 +5,7 @@ import { NanoNode } from 'nano-account-crawler/dist/nano-node';
 import { parseSupplyRepresentative } from "./block-parsers/supply";
 import { CANCEL_SUPPLY_REPRESENTATIVE, MAX_RPC_ITERATIONS } from "./constants";
 
-// Crawler to find all mint blocks for a specific supply block
+// Crawler to find all supply blocks by an issuer
 export class SupplyBlocksCrawler {
   private _issuer: string;
   private _nanoNode: NanoNode;
@@ -16,30 +16,49 @@ export class SupplyBlocksCrawler {
   }
 
   async crawl(): Promise<INanoBlock[]> {
+    // Initialize crawler that crawls backward from account frontier
     const banCrawler = new NanoAccountBackwardCrawler(this._nanoNode, this._issuer);
     await banCrawler.initialize();
     banCrawler.maxRpcIterations = MAX_RPC_ITERATIONS;
 
     const supplyBlocks: INanoBlock[] = [];
-    let followingBlock: INanoBlock = undefined;
+    let followedByBlock: INanoBlock = undefined;
 
-    // Crawl forward in issuer account from supply block
+    // Crawl backward from frontier in issuer account
     for await (const block of banCrawler) {
-      const supplyData = parseSupplyRepresentative(block.representative);
-      
-      // if there's no subsequent block, mint or cancel, don't add the supply block to the list yet.
-      if (supplyData && followingBlock !== undefined) {
-        const followedByCancelBlock = followingBlock.representative == CANCEL_SUPPLY_REPRESENTATIVE;
-        const followedBySupplyBlock = parseSupplyRepresentative(followingBlock.representative);
-        
-        if (!followedByCancelBlock && !followedBySupplyBlock) {
-          supplyBlocks.push(block);
-        }
+      if (this.validateSupplyBlock(block, followedByBlock)) {
+        supplyBlocks.push(block);
       }
 
-      followingBlock = block;
+      // Cache followedByBlock that is ahead of block in next iteration
+      followedByBlock = block;
     }
 
     return supplyBlocks;
+  }
+
+  // https://github.com/Airtune/73-meta-tokens/blob/main/meta_ledger_protocol/supply_block.md#validation
+  private validateSupplyBlock(block: INanoBlock, followedByBlock: INanoBlock) {
+    // supply block isn't confirmed if it's the frontier of the account
+    if (followedByBlock === undefined) {
+      return false;
+    }
+    
+    // invalid if followed by a #cancel_supply block
+    if (followedByBlock.representative === CANCEL_SUPPLY_REPRESENTATIVE) {
+      return false;
+    }
+
+    // invalid if followed by a #supply block
+    if (parseSupplyRepresentative(followedByBlock.representative)) {
+      return false;
+    }
+
+    const supplyData = parseSupplyRepresentative(block.representative);
+    if (supplyData) {
+      return true;
+    } else {
+      return false;
+    }
   }
 }
