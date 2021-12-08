e18e4a62bbb2e2fa4666ebff3635d0f7f514431b
Airtune <airtune@protonmail.com>
2021-12-08 04:05:25 +0100

    Verify representative is unchanged for receive#atomic_swap

--- src/asset-crawler-states/pending-atomic-swap.ts
+++ src/asset-crawler-states/pending-atomic-swap.ts
@@ -5,15 +5,19 @@ import { NanoAccountForwardCrawler } from "nano-account-crawler/dist/nano-accoun
 import { AssetCrawler } from "../asset-crawler";
 import { parseAtomicSwapRepresentative } from "../block-parsers/atomic-swap";
 import { findBlockAtHeight } from "../lib/find-block-at-height";
+import { findBlockAtHeightAndPreviousBlock } from "../lib/find-block-at-height-and-previous-block";
+import { INanoBlock } from "nano-account-crawler/dist/nano-interfaces";
 
 export async function pendingAddNextMetaBlock(assetCrawler: AssetCrawler): Promise<boolean> {
   const paymentAccount = assetCrawler.frontier.account;
   const paymentHeight = BigInt(assetCrawler.frontier.nanoBlock.height) + BigInt(1);
-  const nextBlock = await findBlockAtHeight(paymentAccount, paymentHeight);
+  const [previousBlock, nextBlock]: [INanoBlock, INanoBlock] = await findBlockAtHeightAndPreviousBlock(paymentAccount, paymentHeight);
   const sendAtomicSwap = assetCrawler.assetChain[assetCrawler.assetChain.length - 2];
 
   // guards
-  if (typeof nextBlock === 'undefined') { return false; }
+  if (typeof nextBlock === 'undefined') {
+    return false;
+  }
   if (sendAtomicSwap.state !== 'send_atomic_swap') {
     throw Error(`UnexpectedMetaChain: Expected states of the chain to be send_atomic_swap -> pending_atomic_swap -> ... Got: ${sendAtomicSwap.state} -> ${assetCrawler.frontier.state} -> ...`);
   }
@@ -23,8 +27,9 @@ export async function pendingAddNextMetaBlock(assetCrawler: AssetCrawler): Promi
   const atomicSwapConditions: IAtomicSwapConditions = parseAtomicSwapRepresentative(sendAtomicSwap.nanoBlock.representative);
   const payedEnough = BigInt(nextBlock.amount) >= atomicSwapConditions.minRaw;
   const payedCorrectAccount = nextBlock.account === sendAtomicSwap.account;
+  const representativeUnchanged = nextBlock.representative === previousBlock.representative;
 
-  if (nextBlock.subtype === 'send' && payedCorrectAccount && payedEnough) {
+  if (nextBlock.subtype === 'send' && payedCorrectAccount && payedEnough && representativeUnchanged) {
     assetCrawler.assetChain.push({
       state: 'ownership',
       type: 'send#payment',
