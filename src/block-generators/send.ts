aa6ab2e1bfd48e0a9cea6b4aa466cfdc5a26bd3e
MajicGit <95971895+MajicGit@users.noreply.github.com>
2022-01-09 20:15:10 +0100

    Implemented send block generator  (#1)
    * Implemented send block generator
    
    Implemented basics for being able to create a send block to transfer NFTs.
    
    * Removed redundant imports

--- src/block-generators/send.ts
+++ src/block-generators/send.ts
@@ -1,3 +1,22 @@
-export const generateSendAssetBlock = (assetRepresentative: string) => {
+import * as bananojs from '@bananocoin/bananojs';
+import { generateBananoSendBlock } from './banano-send';
+import { getAccountInfo } from '../lib/get-account-info';
+
+
+// https://github.com/Airtune/73-meta-tokens/blob/main/meta_ledger_protocol/send_and_receive_assets.md 
+export const generateSendAssetBlock = async (assetRepresentative: string, sender: string, recipient: string) => {
+    const sendRaw = "1";
+    const representative = assetRepresentative;
+    const accountInfo = await getAccountInfo(sender);
+    if (accountInfo == undefined) {
+        throw Error(`Can't retrieve account information.`);
+    }
+    const balanceRaw = accountInfo.balance;
+    if (BigInt(balanceRaw) < BigInt(sendRaw)) {
+        throw Error(`Acount balance is too small.`);
+    }
+    const previous = accountInfo.frontier;
+
+    return generateBananoSendBlock(sender, recipient, BigInt(sendRaw), previous, balanceRaw, representative);
 
 }
