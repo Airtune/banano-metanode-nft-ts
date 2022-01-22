2f9988d789beb9a774d024f451db391c9ad7adae
Airtune <airtune@protonmail.com>
2022-01-22 01:47:14 +0100

    Meta protocol v1.0.0

--- src/constants.ts
+++ src/constants.ts
@@ -1,10 +1,10 @@
 export const META_NODE_VERSION = '0.0.0';
 
-export const META_PROTOCOL_MAJOR = BigInt('0');
-export const META_PROTOCOL_MINOR = BigInt('1');
+export const META_PROTOCOL_MAJOR = BigInt('1');
+export const META_PROTOCOL_MINOR = BigInt('0');
 export const META_PROTOCOL_PATCH = BigInt('0');
 export const META_PROTOCOL_VERSION = `${META_PROTOCOL_MAJOR}.${META_PROTOCOL_MINOR}.${META_PROTOCOL_PATCH}`;
-export const META_PROTOCOL_SUPPORTED_VERSIONS = ["0.1.0"];
+export const META_PROTOCOL_SUPPORTED_VERSIONS = ["1.0.0"];
 
 // Settings
 export const NODE_RPC_URL = 'https://kaliumapi.appditto.com/api'
