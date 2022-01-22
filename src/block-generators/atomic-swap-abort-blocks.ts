29c3b4e6f4feff52a83bf9becf96d6f701fcef2b
Airtune <airtune@protonmail.com>
2022-01-22 04:32:21 +0100

    Update atomic-swap-abort-blocks.ts

--- src/block-generators/atomic-swap-abort-blocks.ts
+++ src/block-generators/atomic-swap-abort-blocks.ts
@@ -8,8 +8,9 @@ import { generateBananoChangeBlock } from "./banano-change";
 // and described as change#abort_receive_atomic_swap and change#abort_payment in the spec:
 // https://github.com/Airtune/73-meta-tokens/blob/main/meta_ledger_protocol/atomic_swap.md
 // They are simply blocks that are invalid for the atomic swap.
-export const generateAtomicSwapAbortBlocks = async (recipientPrivateKey: TPrivateKey, recipient: TAccount, recipientPrevious: TBlockHash, recipientBalanceRaw: bigint, sendRaw: bigint, atomicSwapReceiveBlockHash: TBlockHash, representative: TAccount): Promise<{ abortReceiveAtomicSwapBlock: ISignedBananoBlock, abortPaymentBlock: ISignedBananoBlock }> => {
-  const abortReceiveAtomicSwapBlock: IBananoBlock = generateBananoChangeBlock(recipient, representative, recipientPrevious, recipientBalanceRaw);
+export const generateAtomicSwapAbortBlocks = async (recipientPrivateKey: TPrivateKey, recipient: TAccount, recipientFrontier: TBlockHash, recipientBalanceRaw: bigint, sendRaw: bigint, atomicSwapReceiveBlockHash: TBlockHash, representative: TAccount): Promise<{ abortReceiveAtomicSwapBlock: ISignedBananoBlock, abortPaymentBlock: ISignedBananoBlock }> => {
+  // TODO: Validate abortPaymentBlock representative matches recipientFrontier block's representative
+  const abortReceiveAtomicSwapBlock: IBananoBlock = generateBananoChangeBlock(recipient, representative, recipientFrontier, recipientBalanceRaw);
   const abortPaymentBlock:           IBananoBlock = generateBananoChangeBlock(recipient, representative, atomicSwapReceiveBlockHash, recipientBalanceRaw + sendRaw);
 
   const abortReceiveSignaturePromise = generateSignature(recipientPrivateKey, abortReceiveAtomicSwapBlock);
