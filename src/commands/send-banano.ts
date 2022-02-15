16d3c7b8813041a59bdc473b8a24363b19676274
Airtune <airtune@protonmail.com>
2022-02-16 00:00:04 +0100

    Create send-banano.ts

---
+++ src/commands/send-banano.ts
@@ -0,0 +1,26 @@
+import { AccountCache } from "../account-cache";
+import { generateBananoSendBlock } from "../block-generators/banano-send";
+import { IBananoSend } from "../interfaces/banano-send";
+import { generateSignature } from "../lib/generate-signature";
+import { generateWork } from "../lib/generate-work";
+import { processBlock } from "../lib/process-block";
+import { TAccount, TBlockHash } from "../types/banano";
+
+export const sendBananoCmd = async (accountCache: AccountCache, representative: TAccount, recipient: TAccount, amountRaw: bigint): Promise<TBlockHash> => {
+  const previous: TBlockHash = await accountCache.getFrontier();
+  const balanceRaw: bigint = await accountCache.getBalance();
+  const workPromise: Promise<string> = generateWork(previous);
+
+  // guard
+  if (accountCache.accountState === "supply_awaiting_mint") {
+    throw Error("CmdError: Unexpected send. Following a supply block, you must either make the first mint or cancel the supply block");
+  }
+
+  const block: IBananoSend = generateBananoSendBlock(accountCache.account, recipient, amountRaw, previous, balanceRaw, representative);
+
+  block.signature = await generateSignature(accountCache.privateKey, block);
+  block.work      = await workPromise;
+
+  const blockHash = await processBlock(block, "send", "send Banano");
+  return blockHash;
+};
